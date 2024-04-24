ccdf0ff52f2f333f7fa2d5357f552661a88f715f
Erick Cesar <e.cesar88@gmail.com>
2024-04-24 10:54:21 -0300

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,40 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+  end
+
+  @spec hash_input(String.t()) :: Identicon.Image.t(hex: [byte()])
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+  end
+end
